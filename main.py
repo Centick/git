87b5a936c03b1b21a880502c96e26aad732e7246
Centick <utyatsky.ry@yandex.ru>
2023-11-27 16:07:58 +0300

    commit main 1

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-def main():
-    print("My first git-repo")
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)  # Загружаем дизайн
+        self.pushButton.clicked.connect(self.a)
+
+    def a(self):
+        self.hide()
 
 
 if __name__ == '__main__':
-    main()
-    
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
