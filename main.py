feed4b9fe8ede9224a63bfc891be50e99298c48b
Centick <utyatsky.ry@yandex.ru>
2023-11-27 16:10:08 +0300

    Revert "commit main 1"
    This reverts commit 87b5a936c03b1b21a880502c96e26aad732e7246.

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-import sys
-
-from PyQt5 import uic  # Импортируем uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-
-class MyWidget(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('UI.ui', self)  # Загружаем дизайн
-        self.pushButton.clicked.connect(self.a)
-
-    def a(self):
-        self.hide()
+def main():
+    print("My first git-repo")
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
-    sys.exit(app.exec_())
+    main()
+    
